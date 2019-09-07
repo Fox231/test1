9bc2f1fd269f662445826b1569eb48de2eae90c8
Fox231 <Fireball996@gmail.com>
2019-09-07 15:59:25 +0300

    MyFirstCommit

---
+++ Proto-Site.php
@@ -0,0 +1,39 @@
+<html lang="ru">
+<head>
+    <title>Mork Alder Official News</title>
+    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="pictures.css">
+</head>
+<body>
+    <div class="head">
+        <p>MORK ALDER</p>
+        <div class="test1">
+            <a href="">Music</a>
+            <a href="">Video</a>
+            <a href="">Store</a>
+        </div>
+    </div>
+    <div class="uphead">
+        <p>FILTER:
+            <a href="">ALL</a>
+            <a href="">ALBUMS</a>
+            <a href="">SINGELS</a>
+            <a href="">REMIXES</a></p>
+    </div>
+    <div class="block1">
+        <img src="images/Up left.jpg" id="pict1">
+        <img src="images/Up middle.jpg" id="pict2">
+        <img src="images/Up right.jpg" id="pict3">
+    </div>
+    <div class="block2">
+        <img src="images/Middle Left III.jpg" id="pict4">
+        <img src="images/Middle%20Left%20II.jpg" id="pict5">
+        <img src="images/Middle%20Right%20I.jpg" id="pict6">
+        <img src="images/Middle_RIght_II.jpg" id="pict7">
+    </div>
+    <div class="block3">
+        <img src="images/End.jpg">
+    </div>
+    <footer><p>© 2004 - 2019 Mork Alder. All rights reserved. | Help Wanted | Advertising Info | Contact Us | Terms & Conditions | Privacy Policy |</p></footer>
+</body>
+</html>
